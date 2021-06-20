b3f86e0828b3c2008573b896efd69c4fcdd3c7b2
Glitch (season-quiet-munchkin) <none>
2021-06-20 08:40:07 +0000

    🏖🔔 Checkpoint ./README.md copy:33022968/1208 ./server.js:33022968/625 ./LICENSE:33022968/1303 ./INFORMATION.txt:33022968/176

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ var dbd = require("dbd.js")
 var fs = require("fs")
 
 const bot = new dbd.Bot({
-token: "TOKEN",
+token: "Taruh token bot mu disini", // Tutorial untuk mendapatkan token bot ada di channel ku, PigiKotak
 prefix: "$getServerVar[prefix]"
 })
  
 bot.status({
-  text: "YOUR BOT BIO",
-  type: "PLAYING",
-  time: 12
+  text: "Bio bot kamu", // $serverCount untuk melihat berapa banyak server yang bot kamu masuk | $membersCount untuk melihat total semua member dari semua server yang bot kamu masuk
+  type: "LISTENING", // List: PLAYING, STREAMING, LISTENING, WATCHING
+  time: 12 // Ini akan mengubah bio pertama dengan bio kedua.
 })
  
 bot.onMessage()
@@ -17,7 +17,8 @@ bot.onMessage()
  // Variable
 bot.variables({
  prefix: "YOUR BOT PREFIX",
- warn: "0"
+ warn: "0",
+ color: "FFFFFF"
 })
 
  //commands handler
@@ -26,7 +27,8 @@ for(const file of reader) {
   const command = require(`./commands/${file}`)
   bot.command({
 name: command.name, 
-code: command.code
+code: command.code,
+aliases: command.alias
   })
 }
 
