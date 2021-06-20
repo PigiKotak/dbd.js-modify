dfdb7b3b2d33a31c5b2bfec3a225f735a140eb1a
Glitch (season-quiet-munchkin) <none>
2021-06-20 09:25:41 +0000

    🐁🍶 Checkpoint ./commands/ganti-prefix.js:33022968/79 ./README:33022968/624

--- commands/ganti-prefix.js
+++ commands/ganti-prefix.js
@@ -6,5 +6,6 @@ code:
 `$onlyAdmin[<:Mousea_error:855967666221088779>, **_Untuk melakukan command ini dibutuhkan MOD permission_**]
 $author[Ganti PREFIX]
 $color[$getVar[warna]]
-$description[<a:Mousea_checklist:855820835033579540> - Berhasil! Sekarang PREFIX aku di server ini adalah = `$getServerVar[prefix]`]`
+$description[<a:Mousea_checklist:855820835033579540> - Berhasil! Sekarang PREFIX aku di server ini adalah = \`$getServerVar[prefix]\`]
+$footer[Mousea.co.id JS Bot V1.4 | ]`
 }
